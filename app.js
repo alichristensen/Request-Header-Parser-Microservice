116615eaacb2e5eaf0cca21c1914cbfcd7032871
alichristensen <ali.christensen1@gmail.com>
2017-01-20 09:54:01 -0600

    add start script to package.json

--- app.js
+++ app.js
@@ -9,11 +9,26 @@ var header = {
 }
 
 app.get("/", function(req, res){
-	header.language = req.headers['accept-language'];
-	header.software = req.headers['user-agent'];
-	console.log(header);
+	res.send("FreeCodeCamp Backend API Projects")
+})
+
+app.get("/whoami", function(req, res){
+	header.language = req.headers['accept-language'].slice(0, 5);
+	header.software = softwareParser(req.headers['user-agent']);
+	header.ipaddress = req.connection.remoteAddress ||
+					req.connection.remoteAddress ||
+					req.socket.remoteAddress;
+	if (header.ipaddress.substr(0, 7) == "::ffff:") {
+  		header.ipaddress = header.ipaddress.substr(7)
+	}
 	res.send(header);
 });
+
+function softwareParser(str) {
+	var start = str.indexOf('(');
+	var finish = str.indexOf(')');
+	return (str.slice(start+1, finish));
+}
 
 app.listen(port, function() {
 	console.log("listening at port " + port);
